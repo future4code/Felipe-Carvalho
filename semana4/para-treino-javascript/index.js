7ac8d67a703e0b2ca0387483975f554fb7e08201
felipe <felipesoutomdt@gmail.com>
2021-05-24 16:40:50 -0300

    semana4 aula 14 flex e grid

---
+++ semana4/para-treino-javascript/index.js
@@ -0,0 +1,22 @@
+/* 
+const arrayNumbers = [1, 4, 8, 2, 5, 4, 10, 1, 2, 4, 7, 5] ;
+
+const numeroEscolhido = prompt("Informe o número para pesquisar no array: ");
+
+function contaOcorrencias(arrayDeNumeros, numeroEscolhido) {
+    let quantidade = 0;
+    
+    for(i=0; i < arrayDeNumeros.length; i++){
+            console.log(arrayDeNumeros[i])
+            console.log(numeroEscolhido)
+        if(arrayDeNumeros[i] === numeroEscolhido){
+           console.log("pq vc nao entra aqui?")
+        } 
+        // return console.log(`O numero ${numeroEscolhido} aparece ${quantidade}x`)
+    }
+   return
+    console.log(numeroEscolhido, quantidade)
+
+}
+
+contaOcorrencias(arrayNumbers, numeroEscolhido); */
