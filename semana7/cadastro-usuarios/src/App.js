e14a1a37ea83b8678f7dbb1ab9f7aa14096ebff8
felipe <felipesoutomdt@gmail.com>
2021-06-15 21:52:51 -0300

    finalizando projeto

--- semana7/cadastro-usuarios/src/App.js
+++ semana7/cadastro-usuarios/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {createGlobalStyle} from 'styled-components';
-import Register from './components/Register/Register';
 import axios from "axios";
+import Register from './components/Register/Register';
+import List from './components/List/List'
 
 const GlobalStyle = createGlobalStyle`
  body{
@@ -9,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
    display: flex;
    align-items: center;
    justify-content: center;
+   background-color: #323C61;
  } 
 `
 const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
@@ -20,25 +22,66 @@ const headers = {
 
 class App extends React.Component {
 
+  state = {
+    listNames: [],
+    inputName: "",
+    inputEmail: ""
+  }
+
   componentDidMount() {
     this.getList()
   }
 
+  onChangeInputName = (event) => {
+    this.setState({inputName: event.target.value})
+  }
+
+  onChangeInputEmail = ((event) => {
+    this.setState({inputEmail: event.target.value})
+  })
+
   getList = () => {
     axios.get(url, headers)
     .then((res) => {
-      console.log(res)
+      this.setState({listNames: res.data});
     })
     .catch((err) => {
-      console.log(err.response.data)
+      alert(err.response.data.message);
     }) 
   }
 
+  createUser = () => {
+    const body = {
+      name: this.state.inputName,
+      email: this.state.inputEmail
+    }
+    axios.post(url, body, headers)
+      .then(() => {
+        alert("Cadastro  realizado com sucesso");
+        this.setState({inputName: ""});
+        this.setState({inputEmail: ""});
+        this.getList()
+        
+      })
+      .catch((err) => {
+        alert(err.response.data.message)
+      })
+  }
+
   render(){
+
     return (
       <div>
         <GlobalStyle /> 
-        <Register />
+        <Register 
+          inputName = {this.state.inputName}
+          onChangeInputName = {this.onChangeInputName}  
+          inputEmail = {this.state.inputEmail}
+          onChangeInputEmail = {this.onChangeInputEmail}
+          createUser = {this.createUser}
+        />
+        <List listNames = {this.state.listNames}/>
+        
       </div>
     );
   }  
--- semana7/cadastro-usuarios/src/components/Register/Register.js
+++ semana7/cadastro-usuarios/src/components/Register/Register.js
@@ -8,26 +8,51 @@ const RegisterContainer = styled.div`
     align-items: center;
     padding: 35px;
     border: 1px solid black;
+    background: #4F5361;
+    margin-top: 200px;
 `
 
 const RegisterLabel = styled.label`
     padding: 15px 0;
+    color: #BCC5E6;
+`
+
+const InputStyle = styled.input`
+    color: #4F5361;
+    background-color: #BCC5E6;
+`
+
+const ButtonStyle = styled.button`
+    color: #323C61;
+    background-color: #BCC5E6;
+    border: none;
+    width: 4rem;
+    height: 2rem;
 `
 
 class Register extends React.Component {
+
     render() {
         return (
             <div>
                 <RegisterContainer>
                     <RegisterLabel htmlFor="">
                         Nome: 
-                        <input type="text" />
+                        <InputStyle 
+                            type="text" 
+                            value={this.props.inputName}
+                            onChange={this.props.onChangeInputName}
+                        />
                     </RegisterLabel>
                     <RegisterLabel htmlFor="">
                         Email: 
-                        <input type="text" />
+                        <InputStyle 
+                            type="text"
+                            value= {this.props.inputEmail}
+                            onChange= {this.props.onChangeInputEmail}
+                        />
                     </RegisterLabel>
-                    <button>Salvar</button>
+                    <ButtonStyle onClick={this.props.createUser}>Salvar</ButtonStyle>
                 </RegisterContainer>
             </div>
         )
